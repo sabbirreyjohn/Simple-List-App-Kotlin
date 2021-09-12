cec66141fa16c6a78337dc74d87bffb9db7b11aa
khis@stibodx.com <khis@stibodx.com>
2021-09-12 15:40:11 +0600

    add some layer on both remote and local db api.

--- app/src/main/java/xyz/androidrey/basiclistapp/datasource/Retrofit.kt
+++ app/src/main/java/xyz/androidrey/basiclistapp/datasource/Retrofit.kt
@@ -1,13 +1,12 @@
 package xyz.androidrey.basiclistapp.datasource
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import xyz.androidrey.basiclistapp.model.Picture
+import javax.inject.Inject
 
 private const val BASE_URL =
     "https://jsonplaceholder.typicode.com"
@@ -20,11 +19,23 @@ private val retrofit =
 
 interface PictureInterface {
     @GET("photos")
-    suspend fun getPictures(): List<Picture>
+    suspend fun getPictures(): List<Picture>?
 }
 
+/*
 object PictureApi {
     val pictureInterface: PictureInterface by lazy {
         retrofit.create(PictureInterface::class.java)
     }
+}*/
+
+class PictureApiHelperImpl @Inject
+internal constructor(val pictureInterface: PictureInterface) : PictureApiHelper{
+    override suspend fun getPictures(): List<Picture>? {
+        return pictureInterface.getPictures()
+    }
+}
+
+interface PictureApiHelper{
+    suspend fun getPictures(): List<Picture>?
 }
